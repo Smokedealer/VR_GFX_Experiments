dfbaea60f979c27772280714a02dced534b2cf36
Matěj Kareš <karesmatej93@gmail.com>
2018-04-02 17:02:46 +0200

    sync

--- DIP/Assets/_Scripts/Recording/PointInTime.cs
+++ DIP/Assets/_Scripts/Recording/PointInTime.cs
@@ -1,4 +1,13 @@
-﻿public class PointInTime
+﻿using UnityEngine;
+
+public class PointInTime
 {
-    
+    private Vector3 _transform;
+    private Quaternion _quaternion;
+
+    public PointInTime(Vector3 transform, Quaternion quaternion)
+    {
+        _transform = transform;
+        _quaternion = quaternion;
+    }
 }
---
+++ DIP/Assets/_Scripts/Recording/Recorder.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Recorder : MonoBehaviour
+{
+	public GameObject player;
+
+	private List<PointInTime> playerRecording;
+	
+	// Use this for initialization
+	void Start () {
+		playerRecording = new List<PointInTime>();	
+	}
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		playerRecording.Add(new PointInTime(player.transform.position, player.transform.rotation));
+
+		if (playerRecording.Count % 1000 == 0)
+		{
+			Debug.Log("Recording size: " + playerRecording.Count);
+		}
+	}
+}
